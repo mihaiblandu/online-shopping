0d13c59381a877ba0c5ef70a4e72f9b468f13688
mihai blandu <mihaiblandu@icloud.com>
2018-01-19 10:57:40 +0200

    first commit

---
+++ shoppingbackend/src/main/java/net/mihai/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.mihai.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
