15922b08b7b756ea2124b7ccf6797de9d9cd51c3
mihai blandu <mihaiblandu@icloud.com>
2018-02-13 19:41:49 +0200

    ok json is working

--- shoppingbackend/src/main/java/net/mihai/shoppingbackend/dao/CategoryDAO.java
+++ shoppingbackend/src/main/java/net/mihai/shoppingbackend/dao/CategoryDAO.java
@@ -11,8 +11,8 @@ public interface CategoryDAO {
 	Category get(int id);
 	List<Category> list();
 	boolean add(Category category);
-//	boolean update(Category category);
-//	boolean delete(Category category);
+	boolean update(Category category);
+	boolean delete(Category category);
 	
 	
 }
